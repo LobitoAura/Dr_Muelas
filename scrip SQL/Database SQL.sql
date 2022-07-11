32ac52c96e127b671a10fcbf18c63a2b51312cbd
Kevin <kevin_ndelgado11@outlook.com>
2022-07-10 21:34:25 -0300

    Actualizado database scrip SQL

--- scrip SQL/Database SQL.sql
+++ scrip SQL/Database SQL.sql
@@ -18,29 +18,23 @@ CREATE TABLE `Persona`
 PRIMARY KEY (`dni`)
 ) ENGINE=INNODB;
 
-CREATE TABLE `Turno`
-(
- `id`             int NOT NULL AUTO_INCREMENT ,
- `categoria`      boolean NOT NULL ,
- `fecha`          varchar(45) NOT NULL ,
- `disponibilidad` boolean NOT NULL ,
+CREATE TABLE `turno` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `categoria` tinyint(1) NOT NULL,
+  `fecha` varchar(45) NOT NULL,
+  `disponibilidad` tinyint(1) NOT NULL,
+  `id_paciente` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
 
-PRIMARY KEY (`id`)
-) ENGINE=INNODB;
-
-CREATE TABLE `Paciente`
-(
- `id`         int NOT NULL AUTO_INCREMENT ,
- `id_dni`     int NOT NULL ,
- `id_turno`   int NOT NULL ,
- `emergencia` boolean NOT NULL ,
-
-PRIMARY KEY (`id`),
-KEY `FK_Persona` (`id_dni`),
-CONSTRAINT `FK_Persona` FOREIGN KEY `FK_Persona` (`id_dni`) REFERENCES `Persona` (`dni`) ON DELETE CASCADE ON UPDATE CASCADE,
-KEY `FK_Turno` (`id_turno`),
-CONSTRAINT `FK_Turno` FOREIGN KEY `FK_Turno` (`id_turno`) REFERENCES `Turno` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
-) ENGINE=INNODB;
+CREATE TABLE `paciente` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_dni` int NOT NULL,
+  `emergencia` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_Persona` (`id_dni`),
+  CONSTRAINT `FK_Persona` FOREIGN KEY (`id_dni`) REFERENCES `persona` (`dni`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
 
 CREATE TABLE `Historia_Clinica`
 (
